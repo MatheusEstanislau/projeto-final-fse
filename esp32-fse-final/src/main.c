f67220598e621cce4fb4678d92162ef23589f5d8
MatheusEstanislau <matheus.estanislau7@gmail.com>
2021-05-15 10:47:05 -0300

    chore: add esp32 inital config

---
+++ esp32-fse-final/src/main.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_http_client.h"
+#include "esp_log.h"
+#include "freertos/semphr.h"
+#include <unistd.h>
+#include "dht/dht11.h"
+#include "mqtt/mqtt.h"
+#include "wifi/wifi.h"
+#include "cJSON.h"
+
+#define AP_SSID "Matheus"
+#define AP_PASSWORD "ame26081996"
+#define AP_MAX_CONN 4
+#define CHANNEL 0
+
+xSemaphoreHandle conexaoWifiSemaphore;
+xSemaphoreHandle conexaoMQTTSemaphore;
+
+int initialized = 0;
+
+void conectadoWifi(void *params)
+{
+  while (true)
+  {
+    if (xSemaphoreTake(conexaoWifiSemaphore, portMAX_DELAY))
+    {
+      mqtt_start();
+    }
+  }
+}
+
+void trataComunicacaoComServidor(void *params)
+{
+  struct dht11_reading dhtreading;
+  DHT11_init(4);
+  cJSON *json;
+  json = cJSON_CreateObject();
+  if (xSemaphoreTake(conexaoMQTTSemaphore, portMAX_DELAY))
+  {
+    while (true)
+    {
+      if (initialized == 0)
+      {
+        cJSON_AddNumberToObject(json, "id", 1);
+        char *string = cJSON_Print(json);
+        mqtt_envia_mensagem("fse2020/150141220/dispositivos/1", string);
+        initialized = 1;
+      }
+      else
+      {
+        dhtreading = DHT11_read();
+        while (dhtreading.status == -1)
+        {
+          dhtreading = DHT11_read();
+        }
+        cJSON *jsonTemperature, *jsonHumidity;
+        jsonTemperature = cJSON_CreateObject();
+        jsonHumidity = cJSON_CreateObject();
+        cJSON_AddNumberToObject(jsonTemperature, "temperature", dhtreading.temperature);
+        cJSON_AddNumberToObject(jsonHumidity, "humidity", dhtreading.humidity);
+        mqtt_envia_mensagem("fse2020/150141220/sala/temperatura", cJSON_Print(jsonTemperature));
+        mqtt_envia_mensagem("fse2020/150141220/sala/umidade", cJSON_Print(jsonHumidity));
+      }
+      vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+  }
+}
+
+void app_main()
+{
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+  {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+
+  conexaoWifiSemaphore = xSemaphoreCreateBinary();
+  conexaoMQTTSemaphore = xSemaphoreCreateBinary();
+  wifi_start();
+
+  xTaskCreate(&conectadoWifi, "Conexão ao MQTT", 4096, NULL, 1, NULL);
+  xTaskCreate(&trataComunicacaoComServidor, "Comunicação com Broker", 4096, NULL, 1, NULL);
+}
